9e2d89fcb06ea97c3702a2b83db22dbda8f2b048
DylanMsK <kms920612@gmail.com>
2019-05-23 15:55:40 +0900

    페이지 너비 조절

--- src/components/layout.js
+++ src/components/layout.js
@@ -57,13 +57,17 @@ class Layout extends React.Component {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(24),
+          maxWidth: rhythm(30),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
         <header>{header}</header>
         <main>{children}</main>
-        <footer>
+        <footer
+          style={{
+            textAlign: `center`,
+          }}
+        >
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
